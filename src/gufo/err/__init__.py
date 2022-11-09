06b0e5715515d372e6d0b705cc8bb6dc16a3ca62
Dmitry Volodin <dv@nocproject.org>
2022-11-09 16:07:45 +0100

    Fix flake8

--- src/gufo/err/__init__.py
+++ src/gufo/err/__init__.py
@@ -8,24 +8,22 @@ Human-readable error reporting.
 
 Attributes:
     __version__: Current version.
-    err: Err singletone
-    Err: Err class
     HAS_CODE_POSITION: True, if Python interpreter supports
         exact code positions  (Python 3.11+)
 """
 
 # Gufo Labs modules
 from .types import (
-    ErrorInfo,  # noqa
-    FrameInfo,  # noqa
-    SourceInfo,  # noqa
-    CodePosition,  # noqa
-    Anchor,  # noqa
+    ErrorInfo,
+    FrameInfo,
+    SourceInfo,
+    CodePosition,
+    Anchor,
 )
-from .frame import iter_frames, exc_traceback, HAS_CODE_POSITION  # noqa
-from .logger import logger  # noqa
-from .err import Err, err  # noqa
-from .abc.failfast import BaseFailFast  # noqa
-from .abc.middleware import BaseMiddleware  # noqa
+from .frame import iter_frames, exc_traceback, HAS_CODE_POSITION
+from .logger import logger
+from .err import Err, err
+from .abc.failfast import BaseFailFast
+from .abc.middleware import BaseMiddleware
 
 __version__: str = "0.2.0"
